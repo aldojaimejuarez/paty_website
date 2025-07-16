c9421a3dbad2d5ed02ba1d54f51a71e27dc47eef
aldojaimejuarez <aldojaimejuarez@gmail.com>
2025-07-16 17:33:23 +1000

    paty_website

--- vite.config.ts
+++ vite.config.ts
@@ -3,6 +3,7 @@ import react from '@vitejs/plugin-react';
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  base: '/paty_website/', // ✅ importante para GitHub Pages
   plugins: [react()],
   optimizeDeps: {
     exclude: ['lucide-react'],
